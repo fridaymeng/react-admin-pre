c611c1bff1d2d8c29f845380a62c41661d693cd5
haikun.meng <haikun.meng@udai.work>
2021-02-08 20:17:30 +0800

    save

--- src/views/list/Index.js
+++ src/views/list/Index.js
@@ -1,9 +1,29 @@
-function App() {
-  return (
-    <div className="App">
-      List
-    </div>
-  );
+import React from "react";
+class HelloMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      second: 0,
+    };
+  }
+
+  tick() {
+    this.setState((state) => ({
+      seconds: state.seconds + 1,
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return <div>{this.state.second}</div>;
+  }
 }
 
-export default App;
+export default HelloMessage;
